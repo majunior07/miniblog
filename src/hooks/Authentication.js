dd970428ebfe448221ffd9e4ec5229d00db24bd2
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-08-04 19:57:49 -0300

    Terceiro commit do dia 04/08/2023

--- src/hooks/Authentication.js
+++ src/hooks/Authentication.js
@@ -86,6 +86,7 @@ export const useAuthentication = () => {
         try {
 
             await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false);
 
         } catch(error) {
 
@@ -93,8 +94,14 @@ export const useAuthentication = () => {
 
             if(error.message.includes("user-not-found")) {
                 systemErrorMessage = "Usuário não encontrado."
+            } else if(error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta."
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
             }
 
+            setError(systemErrorMessage);
+            setLoading(false);
         }
     }
 
