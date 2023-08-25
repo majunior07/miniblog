6deb3449fb11a62ab70fc66d1e64156cecc1c344
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-08-25 06:20:28 -0300

    Primeiro commit do dia 25/08/2023

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,35 @@
+// CSS
 import styles from './Home.module.css';
 
+// hooks
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+
+// components
+
+
 const Home = () => {
+
+    const [query, setQuery] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Veja os nossos posts mais recentes</h1>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type='text' 
+                    placeholder='Ou busque por tags...' 
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button className='btn btn-dark'>Pesquisar</button>
+            </form>
+            <div>
+                <h1>Posts...</h1>
+            </div>
         </div>
     );
 };
