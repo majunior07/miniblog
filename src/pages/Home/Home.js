14462a32dd4b742ecdc10a3846b5667e0b80cd1d
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-08-25 16:10:18 -0300

    Segundo commit do dia 25/08/2023

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,17 +7,17 @@ import { useState } from 'react';
 
 // components
 
-
 const Home = () => {
 
     const [query, setQuery] = useState("");
+    const [posts] = useState([])
 
     const handleSubmit = (e) => {
         e.preventDefault()
     }
 
     return(
-        <div>
+        <div className={styles.home}>
             <h1>Veja os nossos posts mais recentes</h1>
             <form onSubmit={handleSubmit}>
                 <input 
@@ -29,6 +29,14 @@ const Home = () => {
             </form>
             <div>
                 <h1>Posts...</h1>
+                {posts && posts.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>Não foram encontrados posts</p>
+                        <Link to="/posts/create" className='btn' >
+                            Criar primeiro post
+                        </Link>
+                    </div>
+                )}
             </div>
         </div>
     );
