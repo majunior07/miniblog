8091f49b3eeaedb91296b82f1395b80c2d557f23
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-10-24 16:20:59 -0300

    Primeiro commit do dia 24/10/2023

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Search from './pages/Search/Search'
+import Search from './pages/Search/Search';
 
 function App() {
 
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+// hooks
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useQuery } from "../../hooks/useQuery";
+
 const Search = () => {
-    return <div>Search</div>;
+    const query = useQuery()
+    const search = query.get("q")
+
+    return <div>
+        <h2>Search</h2>
+        <p>{search}</p>
+    </div>;
 };
 
 export default Search;
