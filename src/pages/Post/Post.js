20fb8406d59814ece3c3a763d86666c9eef253e9
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-10-31 16:19:00 -0300

    Primeiro commit do dia 31/10/2023

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -9,13 +9,25 @@ import { useFetchDocument } from "../../hooks/useFetchDocument";
 const Post = () => {
 
     const { id } = useParams();
-    const {document: post} = useFetchDocument("posts", id)
+    const {document: post, loading} = useFetchDocument("posts", id)
 
     return(
-        <div>
+        <div className={styles.post_container}>
+            {loading && <p>Carregando post...</p>}
             {post && (
                 <>
                     <h1>{post.title}</h1>
+                    <img src={post.image} alt={post.title} />
+                    <p>{post.body}</p>
+                    <h3>Este post trata sobre:</h3>
+                    <div className={styles.tags}>
+                        {post.tagsArray.map((tag) => (
+                            <p key={tag}>
+                                <span>#</span>
+                                {tag}
+                            </p>
+                        ))}
+                    </div>
                 </>
             )}
         </div>
