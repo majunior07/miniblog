75c6f125e048611940a7890e4f45a8ec77da51d4
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-11-01 15:14:36 -0300

    Primeiro commit do dia 01/11/2023

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,37 @@
 import styles from './Dashboard.module.css';
 
+import { Link } from 'react-router-dom';
+
+// hooks
+import { useAuthValue } from "../../context/AutfContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+
+    const { user } = useAuthValue();
+    const uid = user.uid;
+
+    
+    const { documents: posts, loading} = useFetchDocuments("posts", null, uid);
+
     return(
         <div>
-            <h1>Dashboard</h1>
+            <h2>Dashboard</h2>
+            <p>Gerencie os seus posts</p>
+            {posts && posts.length === 0 ? (
+                <div className={styles.noposts}>
+                    <p>Não foram encontrados posts</p>
+                    <Link to="/posts/create" className='btn'>
+                        Criar primeiro post
+                    </Link>
+                </div>
+            ) : (
+                <div>
+                    <p>Tem posts!</p>
+                </div>
+            )}
+
+            {posts && posts.map((post) => <h3>{posts.title}</h3>)}
         </div>
     );
 };
