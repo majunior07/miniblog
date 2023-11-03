ea9b3626c1342af669af69b1d4aa6b30a736fa20
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-11-03 16:21:33 -0300

    Segundo commit do dia 03/11/2023

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -14,6 +14,10 @@ const Dashboard = () => {
     
     const { documents: posts, loading} = useFetchDocuments("posts", null, uid);
 
+    const deleteDocument = (id) => {
+
+    }
+
     return(
         <div>
             <h2>Dashboard</h2>
@@ -26,12 +30,33 @@ const Dashboard = () => {
                     </Link>
                 </div>
             ) : (
-                <div>
-                    <p>Tem posts!</p>
-                </div>
-            )}
+                <>
+                    <div>
+                        <span>Título</span>
+                        <span>Ações</span>
+                    </div>
 
-            {posts && posts.map((post) => <h3>{post.title}</h3>)}
+                    {posts.map((post) => (
+                        <div key={post.id}>
+                            <p>{post.title}</p>
+                            <div>
+                                <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                                    Ver
+                                </Link>
+                                <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+                                    Editar
+                                </Link>
+                                <button 
+                                    onClick={() => deleteDocument(post.id)}
+                                    className="btn btn-outline btn-danger"
+                                >
+                                    Excluir
+                                </button>
+                            </div>
+                    </div>
+                    ))}
+                </>
+            )}            
         </div>
     );
 };
